bad0071ea2b1a4d34e3fe3f625a394de26c6a93c
Pingvu <89245290+BaoLanNguyen@users.noreply.github.com>
2022-09-16 14:50:36 +0200

    kkk

--- Abel K uppgift 2.13/Program.cs
+++ Abel K uppgift 2.13/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Uppgift_2._13
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lön för Hashim?");
+            int Hashim = int.Parse(Console.ReadLine());
+            Console.WriteLine("lön för Mire?");
+            int Mire = int.Parse(Console.ReadLine());
+            Console.WriteLine("lön för Mustafa?");
+            int Mustafa = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hashim " + Hashim + "kr" + " Mire " + Mire + "kr " + " Mustafa " + Mustafa + "kr ");
+
+            Console.WriteLine("Medellönen för personalen är " + (Hashim + Mire + Mustafa) / 3);
+        }
+    }
+}
+
